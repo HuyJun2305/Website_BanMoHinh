1c1187f3ec9bb9087a05913d9e81f565fb0eeb7a
dat2410 <datnguyen24102002@gmail.com>
2024-11-03 23:19:58 +0700

    Cart

---
+++ API/IRepositories/ICartDetailRepo.cs
@@ -0,0 +1,14 @@
+﻿using Data.Models;
+
+namespace API.IRepositories
+{
+    public interface ICartDetailRepo
+    {
+        Task<List<CartDetail>> GetAllCartDetail();
+        Task<CartDetail> GetCartDetailById(Guid id);
+        Task Create(CartDetail cartdetail);
+        Task Update(CartDetail cartdetail);
+        Task Delete(Guid id);
+        Task SaveChanges();
+    }
+}
---
+++ API/Repositories/CartDetailRepo.cs
@@ -0,0 +1,50 @@
+﻿using API.Data;
+using API.IRepositories;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories
+{
+    public class CartDetailRepo : ICartDetailRepo
+    {
+        private  readonly ApplicationDbContext _context;
+
+        public CartDetailRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Create(CartDetail cartdetail)
+        {
+            if (await GetCartDetailById(cartdetail.Id) != null) throw new DuplicateWaitObjectException($"Product : {cartdetail.Id} is existed!");
+            await _context.CartDetails.AddAsync(cartdetail);
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var cartdetail = await GetCartDetailById(id);
+            if (cartdetail == null) throw new KeyNotFoundException("Not found this Id!");
+            _context.CartDetails.Remove(cartdetail);
+        }
+
+        public async Task<List<CartDetail>> GetAllCartDetail()
+        {
+            return await _context.CartDetails.Include(p => p.Cart).Include(p => p.Product).ToListAsync();
+        }
+
+        public async Task<CartDetail> GetCartDetailById(Guid id)
+        {
+            return await _context.CartDetails.Include(p => p.Cart).Include(p => p.Product).FirstOrDefaultAsync();
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(CartDetail cartdetail)
+        {
+            if (await GetCartDetailById(cartdetail.Id)== null) throw new KeyNotFoundException("Not found this Id!");
+            _context.Entry(cartdetail).State = EntityState.Modified;
+        }
+    }
+}

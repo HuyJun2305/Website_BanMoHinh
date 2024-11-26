196b54774af871f807f86ec4345b493332921da8
tuanlv251104 <letuancute251104@gmail.com>
2024-11-26 14:20:02 +0700

    CRUD ảnh của sản phẩm
    all

--- View/Controllers/ImageController.cs
+++ View/Controllers/ImageController.cs
@@ -17,15 +17,98 @@ namespace View.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var products = _productServices.GetAllProduct().Result;
-            if (products == null) return View("'Product is null!'");
-            var viewContext = await _imageServices.GetAllImages();
-            if (viewContext == null || !viewContext.Any())
+            ViewData["ProductId"] = new SelectList(await _productServices.GetAllProduct(), "Id", "Name");
+            var viewImage = await _imageServices.GetAllImages();
+            if(viewImage == null || !viewImage.Any())
             {
                 ViewBag.Message = "No product details found.";
                 return View(new List<Image>());
+            } 
+            return View(viewImage);
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var image = _imageServices.GetImageById(id).Result;
+            return View(image);
+        }
+        public IActionResult Create()
+        {
+            ViewData["ProductId"] = new SelectList(_productServices.GetAllProduct().Result, "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("URL ,ProductId")] Image image)
+        {
+            if(image.Id != null)
+            {
+                image.Id = Guid.NewGuid();
+                await _imageServices.Create(image);
+                return RedirectToAction(nameof(Index));
             }
-            return View(viewContext);
+            return View(image);
+        }
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            if(_imageServices.GetAllImages()== null)
+            {
+                return NotFound();
+            }
+            var img = await _imageServices.GetImageById(id);
+            if(img == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductId"] = new SelectList(_productServices.GetAllProduct().Result, "Id", "Name");
+            return View(img);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("URL ,ProductId")] Image image)
+        {
+            if(id != image.Id)
+            {
+                return NotFound();
+            }
+            if(image.Id != null)
+            {
+                try
+                {
+                    await _imageServices.Update(image);
+                }
+                catch (Exception ex)
+                {
+                    return Content(ex.Message);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_productServices.GetAllProduct().Result, "Id", "Name");
+            return View(image);
+        }
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            if(_imageServices.GetAllImages()== null)
+            {
+                return NotFound();
+            }
+            var img = await _imageServices.GetImageById(id);
+            if(img == null) 
+            {
+                return NotFound();
+            }
+            return View(img);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_imageServices.GetAllImages() == null)
+            {
+                return Problem("Entity set 'Image'  is null.");
+            }
+            await _imageServices.Delete(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }

e9a1ad40466a2d64aa1fc47e41f7d52e80207030
Đỗ Huy Hoàng <subaen255@gmail.com>
2024-12-02 12:12:46 +0700

    merge
    megre demo to branch

--- View/Controllers/AddresController.cs
+++ View/Controllers/AddresController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using View.IServices;
 using View.Servicecs;
 using View.ViewModels;
@@ -41,11 +43,34 @@ namespace View.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind("provinces, District, wards, AddressDetail, Description")] Address address)
         {
+            
             if (address.Id != null)
             {
                 address.Id = Guid.NewGuid();
-                address.AccountId = Guid.NewGuid();
-                await _services.Create(address);
+                var jwtToken = HttpContext.Session.GetString("jwtToken"); // Lấy JWT từ session
+
+                if (!string.IsNullOrEmpty(jwtToken))
+                {
+                    var tokenHandler = new JwtSecurityTokenHandler();
+
+                    // Kiểm tra xem token có hợp lệ không
+                    if (tokenHandler.CanReadToken(jwtToken))
+                    {
+                        var jwt = tokenHandler.ReadJwtToken(jwtToken);
+
+                        // Lấy claim chứa userId từ token
+                        var userIdClaim = jwt.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+                        if (Guid.TryParse(userIdClaim, out Guid userId))
+                        {
+                            address.AccountId = userId;
+
+                            await _services.Create(address);
+                        }
+                        return View(address);
+                    }
+                }
+                
                 return RedirectToAction(nameof(Index));
             }
             return RedirectToAction(nameof(Index));
